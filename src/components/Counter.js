48a62f3e0c8856d7f167e2401934b25b9477eb12
Nataliya Pasichnyk <nataliya.pasichnyk@codility.com>
2022-11-26 16:50:05 +0100

    Add Counter component

---
+++ src/components/Counter.js
@@ -0,0 +1,34 @@
+import {useState} from "react";
+
+const Counter = () => {
+    const [counter, setCounter] = useState(0);
+
+  //increase counter
+  const increase = () => {
+    setCounter(count => count + 1);
+  };
+
+  //decrease counter
+  const decrease = () => {
+    setCounter(count => count - 1);
+  };
+
+  //reset counter
+  const reset = () =>{
+    setCounter(0)
+  }
+
+  return (
+    <div>
+      <h1>Counter</h1>
+      <span>{counter}</span>
+      <div>
+        <button onClick={increase}>+</button>
+        <button onClick={decrease}>-</button>
+        <button onClick={reset}>Reset</button>
+      </div>
+    </div>
+  );
+}
+
+export default Counter;
