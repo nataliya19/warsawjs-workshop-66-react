fd018f7c631863cf7bab068cc4e39ac26214f6d5
Nataliya Pasichnyk <nataliya.pasichnyk@codility.com>
2022-11-26 16:57:59 +0100

    Make the minimum value 0

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,32 +3,35 @@ import {useState} from "react";
 const Counter = () => {
     const [counter, setCounter] = useState(0);
 
-  //increase counter
-  const increase = () => {
-    setCounter(count => count + 1);
-  };
+    //increase counter
+    const increase = () => {
+        setCounter(count => count + 1);
+    };
 
-  //decrease counter
-  const decrease = () => {
-    setCounter(count => count - 1);
-  };
+    //decrease counter, make the minimum value 0
+    const decrease = () => {
+        if (counter > 0) {
+            setCounter(count => count - 1);
+        }
+    };
 
-  //reset counter
-  const reset = () =>{
-    setCounter(0)
-  }
 
-  return (
-    <div>
-      <h1>Counter</h1>
-      <span>{counter}</span>
-      <div>
-        <button onClick={increase}>+</button>
-        <button onClick={decrease}>-</button>
-        <button onClick={reset}>Reset</button>
-      </div>
-    </div>
-  );
+    //reset counter
+    const reset = () =>{
+        setCounter(0)
+    }
+
+    return (
+        <div>
+          <h1>Counter</h1>
+          <span>{counter}</span>
+          <div>
+            <button onClick={increase}>+</button>
+            <button onClick={decrease}>-</button>
+            <button onClick={reset}>Reset</button>
+          </div>
+        </div>
+    );
 }
 
 export default Counter;
