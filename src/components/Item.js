4b0d4686e5a61271330f3fb6ffd12984657d3af3
Nataliya Pasichnyk <nataliya.pasichnyk@codility.com>
2022-11-26 21:25:50 +0100

    Add Item component

---
+++ src/components/Item.js
@@ -0,0 +1,9 @@
+const Item = ({todo}) => {
+    return (
+        <div>
+            {todo.task}
+        </div>
+    )
+}
+
+export default Item;
--- src/components/List.js
+++ src/components/List.js
@@ -1,15 +1,15 @@
 import data from "../data.json";
 import {useState} from "react";
+import Item from "./Item";
 
 
 const List = () => {
       const [ toDoList, setToDoList ] = useState(data);
 
       return (<div>
-             {toDoList.map(todo => {
-              return <div>{todo.task}</div>
-           })}
+             {toDoList.map(todo => <Item key={todo.id} todo={todo}/>)}
       </div>)
 }
+
 
 export default List;
