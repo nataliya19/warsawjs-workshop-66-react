5946e7884ba4bd5b785479231719a7a1a325b8e1
Nataliya Pasichnyk <nataliya.pasichnyk@codility.com>
2022-11-26 22:05:23 +0100

    Add new task

--- src/components/List.js
+++ src/components/List.js
@@ -1,22 +1,47 @@
 import data from "../data.json";
-import {useState} from "react";
+import { useState } from "react";
 import Item from "./Item";
-
+import Form from "./Form";
 
 const List = () => {
-      const [toDoList, setToDoList] = useState(data);
+  const [toDoList, setToDoList] = useState(data);
 
-      const handleToggle = (id) => {
-            let mapped = toDoList.map(task => {
-                  return task.id === Number(id) ? {...task, complete: !task.complete} : {...task};
-            });
-            setToDoList(mapped);
-      }
+  const handleToggle = (id) => {
+    let mapped = toDoList.map((task) => {
+      return task.id === Number(id)
+        ? { ...task, complete: !task.complete }
+        : { ...task };
+    });
+    setToDoList(mapped);
+  };
 
-      return (<div>
-            {toDoList.map(todo => <Item key={todo.id} todo={todo} handleToggle={handleToggle}/>)}
-      </div>)
-}
+  const handleFilter = () => {
+    let filtered = toDoList.filter((task) => {
+      return !task.complete;
+    });
+    setToDoList(filtered);
+  };
 
+  const addNewTask = (userInput) => {
+    let newToDos = [
+      ...toDoList,
+      { id: toDoList.length + 1, task: userInput, complete: false },
+    ];
+    setToDoList(newToDos);
+  };
+
+  return (
+    <div>
+      <Form addNewTask={addNewTask}></Form>
+      <div>
+        {toDoList.map((todo) => (
+          <Item key={todo.id} todo={todo} handleToggle={handleToggle} />
+        ))}
+      </div>
+      <br />
+      <button onClick={handleFilter}>Filter completed</button>
+    </div>
+  );
+};
 
 export default List;
